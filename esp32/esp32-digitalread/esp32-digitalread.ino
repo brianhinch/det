834d0ab8ed0dd434e0864eeaad6bc4143ab3e2c3
Brian Hinch <1228677+brianhinch@users.noreply.github.com>
2023-01-28 13:31:27 -0800

    digital read example

---
+++ esp32/esp32-digitalread/esp32-digitalread.ino
@@ -0,0 +1,19 @@
+int i;
+const uint32_t DELAY_TIME = 1;
+
+const int BUTTON_PIN = 17;
+int buttonState;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello computer");
+
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(BUTTON_PIN);
+  Serial.println(buttonState);
+
+  delay(DELAY_TIME);
+}
